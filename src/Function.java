d16526afb5b7fe6ea9f714a062f72698bd807c4c
pjm8331 <petermastropaolo@yahoo.com>
2019-02-28 17:27:06 -0500

    Remade into abstract class

--- src/Function.java
+++ src/Function.java
@@ -1,7 +1,16 @@
-public interface Function {
-    double evaluate(double x);
+public abstract class Function {
+
+    public double evaluate(double x){
+        return 0.0;
+    }
+
     @Override
-    String toString();
-    boolean isConstant();
+    public String toString(){
+        return "";
+    }
+
+    public boolean isConstant(){
+        return false;
+    }
 
 }
