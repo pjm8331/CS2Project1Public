16c297b4b64a6aa90bd414c6d20afd9ce1d89979
pjm8331 <petermastropaolo@yahoo.com>
2019-02-28 18:32:25 -0500

    Sum Complete?

--- src/Sum.java
+++ src/Sum.java
@@ -1,12 +1,28 @@
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Sum extends Function {
-    public Sum(ArrayList<Function> things){
+    public ArrayList<Function> things;
+
+    public Sum(Function ...things){
+        for(Function i: things){
+            this.things.add(i);
+        }
+
 
     }
 
     public double evaluate(double x) {
-        return 0;
+        ArrayList<Double> compute = new ArrayList<>();
+        for(int i = 0; i<things.size(); i++){
+            compute.add(things.get(i).evaluate(x));
+        }
+        double total = 0.0;
+        for(int i = 0; i<compute.size(); i++){
+            System.out.println(total + "+" + compute.get(i)); //For testing purposes
+            total += compute.get(i);
+        }
+        return total;
     }
 
     public boolean isConstant() {
@@ -15,6 +31,10 @@ public class Sum extends Function {
 
     @Override
     public String toString() {
-        return super.toString();
+        String string = "";
+        for(int i = 0; i<things.size(); i++){
+            string += (things.get(i).toString()) + " + ";
+        }
+        return string;
     }
 }
