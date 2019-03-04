700dbffe884b76267ec92d1a0d491f09927d547d
pjm8331 <petermastropaolo@yahoo.com>
2019-03-04 16:23:00 -0500

    Product first try

--- src/Product.java
+++ src/Product.java
@@ -1,2 +1,47 @@
-public class Product {
+import java.util.ArrayList;
+
+public class Product extends Function {
+
+    public Function[] things;
+    private boolean constant = true;
+
+    public Product(Function... things){
+        this.things = things;
+
+        for(Function i : things){
+            if(constant && !i.isConstant()){
+                constant = false;
+            }
+        }
+    }
+
+    public double evaluate(double x) {
+        ArrayList<Double> compute = new ArrayList<>();
+        for(int i = 0; i<things.length; i++){
+            compute.add(things[i].evaluate(x));
+        }
+        double total = 0.0;
+        for(int i = 0; i<compute.size(); i++){
+            total = total * compute.get(i);
+        }
+        return total;
+    }
+
+    public boolean isConstant() {
+        return this.constant;
+    }
+
+    @Override
+    public String toString() {
+        String string = "(";
+        for(int i = 0; i<things.length; i++){
+            if(i == things.length-1){
+                string += things[i].toString() + ")";
+            }
+            else{
+                string += (things[i].toString()) + "*";
+            }
+        }
+        return string;
+    }
 }
