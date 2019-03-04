9733e3d575ff06d2e8f873dd23f29e7605b4f147
pjm8331 <petermastropaolo@yahoo.com>
2019-03-04 17:37:31 -0500

    initial sine creation

--- src/Sine.java
+++ src/Sine.java
@@ -1,2 +1,19 @@
-public class Sine {
+public class Sine extends Function{
+    public Function function;
+    public Sine(Function function){
+        this.function = function;
+    }
+
+    public double evaluate(double x) {
+        return Math.sin(function.evaluate(x));
+    }
+
+    public boolean isConstant() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Sin(" + function.toString() + ")";
+    }
 }
