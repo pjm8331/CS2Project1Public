1255033333f2a7e5c668007368cf9cbea49e5683
pjm8331 <petermastropaolo@yahoo.com>
2019-03-04 17:38:57 -0500

    initial cosine creation

--- src/Cosine.java
+++ src/Cosine.java
@@ -1,2 +1,19 @@
-public class Cosine {
+public class Cosine extends Function{
+    public Function function;
+    public Cosine(Function function){
+        this.function = function;
+    }
+
+    public double evaluate(double x) {
+        return Math.cos(function.evaluate(x));
+    }
+
+    public boolean isConstant() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "Cos(" + function.toString() + ")";
+    }
 }
