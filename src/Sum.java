56d3fab5f4747e2c9884a216c7e362c34a7d072f
pjm8331 <petermastropaolo@yahoo.com>
2019-03-05 15:47:41 -0500

    added simplification to derivative method

--- src/Sum.java
+++ src/Sum.java
@@ -37,11 +37,28 @@ public class Sum extends Function {
         for(int i = 0; i<things.length; i++){
             newthings[i] = things[i].derivative();
         }
-        Sum checks = new Sum(newthings);    //Change to just add all constants and return a sum of added constants and other functions if isConstant is false
+        Sum checks = new Sum(newthings);                    //If all constants, add together and return a constant
         if(checks.isConstant()){
             return new Constant(checks.evaluate(0));
         }
-        return checks;
+        else{                                               //If not complete constant function, add all constants to simplify
+            double add = 0;
+            ArrayList<Function> newerthings = new ArrayList<>();
+            for(int i = 0; i<newthings.length; i++){
+                if(newthings[i].isConstant()){
+                    add += newthings[i].evaluate(0);
+                }
+                else{
+                    newerthings.add(newthings[i]);
+                }
+            }
+            newerthings.add(new Constant(add));
+            Function[] newestthings = new Function[newerthings.size()];
+            for(int i = 0; i<newerthings.size(); i++){
+                newestthings[i] = newerthings.get(i);
+            }
+            return new Sum(newestthings);
+        }
     }
 
     @Override
