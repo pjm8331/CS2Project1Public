7f1bdfaf9016c5323b50407cd6888f564f49be46
pjm8331 <petermastropaolo@yahoo.com>
2019-03-08 17:29:40 -0500

    finished test 1

--- src/FunctionTest1.java
+++ src/FunctionTest1.java
@@ -1,4 +1,11 @@
 import functions.*;
+
+import javax.xml.parsers.SAXParser;
+
+/**
+ * Test class 1 for sums, constants, and variables
+ * @author Peter Mastropaolo(pjm8331)
+ */
 public class FunctionTest1 {
 
 
@@ -8,15 +15,35 @@ public class FunctionTest1 {
             System.exit(1);
         }
         Variable var = Variable.X;
-        System.out.println("Derivative Testing");
-        System.out.println("Testing constants and variables...");
-        Function f1 = new Product(new Constant(2), var, var);
-        System.out.println("Function: " + f1);
-        System.out.println("Derivative: " + f1.derivative());
-        System.out.println("Testing nested functions...");
-        Function f2 = new Product(new Product(new Constant(2), var, var), new Sum(new Product(new Constant(2), var), new Constant(5)));
-        System.out.println("Function: " + f2);
-        System.out.println("Derivative: " + f2.derivative());
+
+        System.out.println("Testing Sums...");
+        System.out.println("Constants...");
+        Function s1 = new Sum(new Constant(4), new Constant(3));
+        System.out.println("S1: " + s1);
+        System.out.println("Evaluate: " + s1.evaluate(0));
+        Function s2 = new Sum(new Constant(4), new Constant(-3));
+        System.out.println("S2: " + s2);
+        System.out.println("Evaluate: " + s2.evaluate(0));
+        Function s3 = new Sum(new Constant(-4), new Constant(-3));
+        System.out.println("S3: " + s3);
+        System.out.println("Evaluate: " + s3.evaluate(0));
+        Function s4 = new Sum(s3, s2, s1);
+        System.out.println("Nested S4: " + s4);
+        System.out.println("Evaluated: " + s4.evaluate(0));
+        System.out.println("Variables...");
+        Function s5 = new Sum(var, var);
+        System.out.println("S5: " + s5 + "  X = 36");
+        System.out.println("Evaluate: " + s5.evaluate(36));
+        Function s6 = new Sum(new Constant(7), var, var);
+        System.out.println("S6: " + s6 + "  X = 21");
+        System.out.println("Evaluate: " + s6.evaluate(21));
+        Function s7 = new Sum(s5, s6);
+        System.out.println("Nested S7: " + s7 + "   X = 3");
+        System.out.println("Evaluate: " + s7.evaluate(3));
+        Function s8 = new Sum(s7, s4);
+        System.out.println("Final Test...");
+        System.out.println("Nested S8: " + s8 + "   X = 8");
+        System.out.println("Evaluate: " + s8.evaluate(8));
     }
 
 }
---
+++ src/FunctionTest2.java
@@ -0,0 +1,21 @@
+import functions.*;
+
+public class FunctionTest2 {
+    public static void main( String[] args ) {
+        if (args.length != 0) {
+            System.err.println("Test1 takes no command line arguments.");
+            System.exit(1);
+        }
+        Variable var = Variable.X;
+
+        System.out.println("Derivative Testing");
+        System.out.println("Testing constants and variables...");
+        Function f1 = new Product(new Constant(2), var, var);
+        System.out.println("Function: " + f1);
+        System.out.println("Derivative: " + f1.derivative());
+        System.out.println("Testing nested functions...");
+        Function f2 = new Product(new Product(new Constant(2), var, var), new Sum(new Product(new Constant(2), var), new Constant(5)));
+        System.out.println("Function: " + f2);
+        System.out.println("Derivative: " + f2.derivative());
+    }
+}
