497d1b8e93a12744b8cdfe457e2cd70ac5846508
pjm8331 <petermastropaolo@yahoo.com>
2019-03-08 14:49:07 -0500

    Cosine docstrings

--- src/functions/Cosine.java
+++ src/functions/Cosine.java
@@ -1,6 +1,7 @@
 package functions;
 
 /**
+ * Class for Cosine functions
  * @author Peter Mastropaolo(pjm8331)
  */
 public class Cosine extends Function{
@@ -9,22 +10,43 @@ public class Cosine extends Function{
         this.function = function;
     }
 
+    /**
+     * evaluates the cosine at the value x
+     * @param x value to evaluate at
+     * @return the resulting value from evaluation
+     */
     public double evaluate(double x) {
         return Math.cos(function.evaluate(x));
     }
 
+    /**
+     * Whether or not the function is constant
+     * @return a boolean value
+     */
     public boolean isConstant() {
         return false;
     }
 
+    /**
+     * Gets the derivative of the Cosine function
+     * @return the derivative of the function
+     */
     public Function derivative(){
         return new Product(new Constant(-1), new Sine(function), function.derivative());
     }
 
+    /**
+     * Simplify function for recursion
+     * @return the given function
+     */
     public Function simplify(){
         return new Cosine(function);
     }
 
+    /**
+     * The function in string form
+     * @return a string of the function
+     */
     @Override
     public String toString() {
         return "Cos(" + function.toString() + ")";
