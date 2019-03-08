c9def5fe9178739b44b0958ff3a23883a950f4e0
pjm8331 <petermastropaolo@yahoo.com>
2019-03-08 14:51:54 -0500

    Function docstrings

--- src/functions/Function.java
+++ src/functions/Function.java
@@ -5,17 +5,45 @@ package functions;
  */
 public abstract class Function {
 
+    /**
+     * evaluates the function at the value
+     * @param x value to evaluate at
+     * @return the value of the function when evaluated
+     */
     public abstract double evaluate(double x);
 
+    /**
+     * A string form of the function
+     * @return the string of the function
+     */
     @Override
     public abstract String toString();
 
+    /**
+     * The derivative of the function
+     * @return the derivative of the function
+     */
     public abstract Function derivative();
 
+    /**
+     * Whether or not the function is constant
+     * @return a boolean value
+     */
     public abstract boolean isConstant();
 
+    /**
+     * Simplify function for recursion
+     * @return simplified function
+     */
     public abstract Function simplify();
 
+    /**
+     * Returns the integral of the function
+     * @param bound1 lower bound
+     * @param bound2 upper bound
+     * @param n amount of intervals for trapezoidal rule
+     * @return the integral of the function
+     */
     public  double integral(double bound1, double bound2, int n){
         double total = 0;
         double avg = (bound2-bound1)/2*n;
